e144e73ef6cb97e30b26cbb55d356b0a8c590ed3
d@tvn <157048397+datvn09@users.noreply.github.com>
2024-01-29 23:32:24 +0700

    Add files via upload

---
+++ REV/    Love Debug/gdb.py
@@ -0,0 +1,22 @@
+import gdb
+import re
+
+file = "love-letter-for-you"
+with open('temp', 'wb') as f:
+    f.write(b'\x03')
+gdb.execute("file {}".format(file))
+readAddr = 0x404272
+gdb.execute(f"b *{readAddr}")
+arrayAddr = 0x406000
+arraySize = 30000
+gdb.execute("r < temp")
+gdb.execute(f"dump memory {file}-dump {hex(arrayAddr)} {hex(arrayAddr + arraySize)}")
+
+with open(f"{file}-dump", 'rb') as f:
+    dump = f.read().replace(b'\x00', b'')
+
+match = re.search(b'uoftctf{(.+?)}', dump)
+print(match.group(0).decode())
+
+gdb.execute("q")
+
